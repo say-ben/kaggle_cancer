e9b2a07960fe1b1623b0b072a41ec7ccb04001e1
say-ben <bensayers@hotmail.com>
2024-12-07 22:54:49 +0000

    Functions for aggregating data and plotting

--- functions.py
+++ functions.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 def aggregate_table(data, factor, columns, aggs):
 
@@ -13,8 +15,29 @@ def aggregate_table(data, factor, columns, aggs):
             tab_new = data[[factor, columns[i]]].groupby(factor).sum()
 
         tab = pd.concat([tab, tab_new], axis=1)
+    
+    tab = tab.reset_index()
 
     return tab
 
 
 
+def plotly_plot(x, y_list, y_names=[None]*10, secondary_y=[False]*10, plot_type=['scatter']*10, colours=None, title=None, x_title=None, y_title=None, y2_title=None, dims=[900,600], x_range=None, y_range=None, y2_range=None):
+    
+    fig = make_subplots(specs=[[{"secondary_y": True}]])
+
+    for i, y in enumerate(y_list):
+        if plot_type[i] == "bar":
+            fig.add_trace(go.Bar(name=y_names[i], x=x, y=y, marker=dict(color=colours[i])), secondary_y=secondary_y[i])
+        if plot_type[i] == "scatter":
+            fig.add_trace(go.Scatter(name=y_names[i], x=x, y=y, marker=dict(color=colours[i])), secondary_y=secondary_y[i])
+
+    fig.update_layout(title_text=title, width=dims[0], height=dims[1])
+    fig.update_xaxes(title_text=x_title, range=x_range)
+    fig.update_yaxes(title_text=y_title, secondary_y=False, range=y_range)
+
+    if y2_title != None:
+        fig.update_yaxes(title_text=y2_title, secondary_y=True, range=y2_range)
+        fig['layout']['yaxis2']['showgrid'] = False
+
+    return fig
